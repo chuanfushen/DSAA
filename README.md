b1588ee1e0ecb9da1e7acb5c8662e7587840f2a7
nohapi <11950016@mail.sustech.edu.cn>
2021-01-16 14:01:50 +0800

    demo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# DSAA
+# DSAA-assignment 1
+The program calculate two integer, output the sum of them. 
+Notice: should put all file together inside same folder.
+
+# step 1
+prepare the test case file(tests.txt), for each test case, seperated by a space.
+
+# step 2
+compile Sum.java by command ::
+    javac Sum.java
+
+# step 3
+excute program by command ::
+    java Sum.java tests.txt
+Then we will get output
---
+++ Sum.java
@@ -0,0 +1,25 @@
+// This Program is to caculate sum of two integer
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Sum{
+
+    public static void main(String[]args){
+        if (args.length <= 0) {
+            System.out.println("Pls input your test case file");
+            return;
+        }
+        try {
+            File file = new File(args[0]);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNext()){
+                int x = scanner.nextInt();
+                int y = scanner.nextInt(); 
+                System.out.format("The sum of %d and %d is: %d\n", x, y, x+y);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
